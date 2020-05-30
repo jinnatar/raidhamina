742572eb31a30f9bcf9a3e85edeebe6c36ca6974
Artanicus <artanicus@nocturnal.fi>
2020-05-30 12:35:16 +0300

    PWA to the max, with a service-worker

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+// service-worker.js
+
+// set names for both precache & runtime cache
+workbox.core.setCacheNameDetails({
+    prefix: 'raidhamina',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'runtime-cache'
+});
+
+// let Service Worker take control of pages ASAP
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+// let Workbox handle our precache list
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+// use `networkFirst` strategy for all shortlinked pages
+workbox.routing.registerRoute(
+    new RegExp('/^[^.]+$/'),
+    workbox.strategies.networkFirst()
+);
+
+// use `networkFirst` strategy for Matomo
+workbox.routing.registerRoute(
+    /^https?:\/\/matomo.nocturnal.fi/,
+    workbox.strategies.networkFirst()
+);
+
+// use `cacheFirst` strategy for images
+workbox.routing.registerRoute(
+    /assets\/(img|icons)/,
+    workbox.strategies.cacheFirst()
+);
+
+// local cdn
+workbox.routing.registerRoute(
+    /^https?:\/\/cdn.nocturnal.fi/,
+    workbox.strategies.staleWhileRevalidate()
+);
+
+// third party files
+workbox.routing.registerRoute(
+    /^https?:\/\/ajax.googleapis.com/,
+    workbox.strategies.staleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+    /^https?:\/\/cdn.datatables.net/,
+    workbox.strategies.staleWhileRevalidate()
+);
+workbox.routing.registerRoute(
+    /^https?:\/\/cdn.jsdelivr.net/,
+    workbox.strategies.staleWhileRevalidate()
+);
