adf35dfebfcc9cd1d2f9b4aef76c991495fd37eb
Artanicus <artanicus@nocturnal.fi>
2020-12-25 10:25:22 +0200

    Populate incident data collection timestamp

--- _plugins/hook-add-last-data-update.rb
+++ _plugins/hook-add-last-data-update.rb
@@ -1,5 +1,7 @@
 Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-  modification_time = File.mtime( '_data/quests.yml' )
-  payload['site']['quests-updated'] = modification_time
+  quest_modification_time = File.mtime( '_data/quests.yml' )
+  payload['site']['quests-updated'] = quest_modification_time
+  incident_modification_time = File.mtime( '_data/incidents.yml' )
+  payload['site']['incidents-updated'] = incident_modification_time
 
 end
