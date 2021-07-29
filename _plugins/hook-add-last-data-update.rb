de1e3b94fb2fcccfa8f82f1d534058aae132665e
Artanicus <artanicus@nocturnal.fi>
2021-07-29 15:49:18 +0300

    Also migrate mtime plugin

--- _plugins/hook-add-last-data-update.rb
+++ _plugins/hook-add-last-data-update.rb
@@ -1,7 +1,7 @@
 Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-  quest_modification_time = File.mtime( '_data/quests.yml' )
+  quest_modification_time = File.mtime( '_data/dyn/quests.yml' )
   payload['site']['quests-updated'] = quest_modification_time
-  incident_modification_time = File.mtime( '_data/incidents.yml' )
+  incident_modification_time = File.mtime( '_data/dyn/incidents.yml' )
   payload['site']['incidents-updated'] = incident_modification_time
 
 end
