9c1d647ccd8ed554c36cb7efc78a61cd2ab493ff
Jinna Kiisuo <jinna+git@nocturnal.fi>
2024-08-19 13:05:52 +0300

    Theoretical support for a Raids view

--- _plugins/hook-add-last-data-update.rb
+++ _plugins/hook-add-last-data-update.rb
@@ -3,5 +3,7 @@ Jekyll::Hooks.register :pages, :pre_render do |page, payload|
   payload['site']['quests-updated'] = quest_modification_time
   incident_modification_time = File.mtime( '_data/dyn/incidents.yml' )
   payload['site']['incidents-updated'] = incident_modification_time
+  raid_modification_time = File.mtime( '_data/dyn/raids.yml' )
+  payload['site']['raids-updated'] = raid_modification_time
 
 end
