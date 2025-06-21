e45df06b2131e9dc1471d605eb3ef4a407fde482
Jinna Kiisuo <jinnak@nocturnal.fi>
2025-06-21 09:51:54 +0300

    strip dyn pages

--- Gemfile
+++ Gemfile
@@ -7,8 +7,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "jekyll"
 gem "minima"
 
-# gem "rails"
-#
 #  Shit that the above should take care of, but don't
 gem "rake"
 gem "csv"
--- _plugins/hook-add-last-data-update.rb
+++
@@ -1,9 +0,0 @@
-Jekyll::Hooks.register :pages, :pre_render do |page, payload|
-  quest_modification_time = File.mtime( '_data/dyn/quests.yml' )
-  payload['site']['quests-updated'] = quest_modification_time
-  incident_modification_time = File.mtime( '_data/dyn/incidents.yml' )
-  payload['site']['incidents-updated'] = incident_modification_time
-  raid_modification_time = File.mtime( '_data/dyn/raids.yml' )
-  payload['site']['raids-updated'] = raid_modification_time
-
-end
